2e7b8d2cbdf6967fef1333e8717c80ecdb2d0dee
Fitrahazahra <116161733+Fitrahazahra@users.noreply.github.com>
2023-10-10 19:04:40 +0700

    Add files via upload

--- main.java
+++ main.java
@@ -1,25 +1,33 @@
-package ISCI100DAY;
+package PITTO;
 
 public class main {
 
     public static void main(String[] args) {
-        // membuat variabel 
-        String nama, alamat;
-        int umur;
-        double tinggi_badan;
 
-        // mengisi variabel
-        nama = "fitrah azahra";
-        alamat = "karossa";
-        umur = 20;
-        tinggi_badan = 152;
+        //perbedaan dari primatif dan non primatif
+        // Tipe data primitif merepresentasikan nilai dasar secara langsung. Misalnya, variabel bertipe int akan langsung menyimpan angka bulat, dan variabel bertipe boolean akan menyimpan true atau false.
+        //Tipe data non-primitif merepresentasikan referensi ke objek yang menyimpan data. Misalnya, variabel bertipe String adalah referensi ke objek String yang berisi teks.
+        // Tipe data primitif
+        int angkaBulat = 35;            // Mendeklarasikan variabel angkaBulat dan menginisialisasinya dengan nilai 35
+        double angkaPecahan = 3.14;     // Mendeklarasikan variabel angkaPecahan dan menginisialisasinya dengan nilai 3.14
+        boolean booleanPrimitif = false; // Mendeklarasikan variabel booleanPrimitif dan menginisialisasinya dengan nilai false
 
-        // mencetak ke layar isi variabel
-        System.out.println("nama : " + nama);
-        System.out.println("alamat : " + alamat);
-        System.out.println("umur : " + umur);
-        System.out.println("tinggi_badan : " + tinggi_badan);
+        // Tipe data non-primitif 
+        String nama = "fitrah";         // Mendeklarasikan variabel nama dan menginisialisasinya dengan string "fitrah"
+        Integer umur = 20;              // Mendeklarasikan variabel umur dan menginisialisasinya dengan objek Integer berisi nilai 20
+        Double tinggi = 152.0;          // Mendeklarasikan variabel tinggi dan menginisialisasinya dengan objek Double berisi nilai 152.0
+        Boolean sudahpunyapacar = false; // Mendeklarasikan variabel sudahpunyapacar dan menginisialisasinya dengan objek Boolean berisi nilai false
 
+        // Menampilkan nilai tipe data primitif
+        System.out.println("Tipe Data Primitif:");
+        System.out.println("Angka bulat: " + angkaBulat);          // Menampilkan nilai variabel angkaBulat
+        System.out.println("Angka Pecahan Primitif: " + angkaPecahan); // Menampilkan nilai variabel angkaPecahan
+        System.out.println("Boolean Primitif: " + booleanPrimitif);   // Menampilkan nilai variabel booleanPrimitif
+
+        // Menampilkan informasi menggunakan tipe data non-primitif
+        System.out.println("Nama: " + nama);                   // Menampilkan nilai variabel nama
+        System.out.println("Umur: " + umur);                   // Menampilkan nilai variabel umur
+        System.out.println("Tinggi: " + tinggi + " cm");       // Menampilkan nilai variabel tinggi
+        System.out.println("Sudah punya pacar: " + sudahpunyapacar); // Menampilkan nilai variabel sudahpunyapacar
     }
-
 }
