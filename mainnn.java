4202db10899e5761ab0a45bd01a4247a9bf7ae30
Fitrahazahra <116161733+Fitrahazahra@users.noreply.github.com>
2023-10-12 20:38:07 +0700

    Add files via upload

---
+++ mainnn.java
@@ -0,0 +1,15 @@
+package pitta;
+
+import java.util.Scanner;
+
+public class mainnn {
+
+    public static void main(String[] args) {
+        String nama;
+        Scanner input = new Scanner(System.in);
+        System.out.println("masukan nama anda :");
+        nama = input.nextLine();
+        System.out.println("nama anda tidak mungkin cicci :");
+    }
+
+}
