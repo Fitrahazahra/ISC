df67106309c7bbef0c22d219f07ed6ef6b9cdcc8
Fitrahazahra <116161733+Fitrahazahra@users.noreply.github.com>
2023-10-13 17:20:52 +0700

    Add files via upload

---
+++ pitta.java
@@ -0,0 +1,17 @@
+package pitta;
+
+public class pitta {
+
+    public static void main(String[] args) {
+        System.out.println("Bilangan 1 = " + (7));
+        System.out.println("Bilangan 2 = " + (2));
+        System.out.println("Hasil operator Artmatika");
+        System.out.println("========================");
+        System.out.println("jumlah = " + (7 + 2));
+        System.out.println("kurang = " + (7 - 2));
+        System.out.println("kali = " + (7 * 2));
+        System.out.println("bagi = " + (7 / 2));
+        System.out.println("sisabagi = " + (7 % 2));
+    }
+
+}
