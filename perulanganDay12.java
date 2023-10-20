4e49a2b24b7e878fa7e88f836b6487d645bb197f
Fitrahazahra <116161733+Fitrahazahra@users.noreply.github.com>
2023-10-20 22:57:55 +0800

    Add files via upload

---
+++ perulanganDay12.java
@@ -0,0 +1,12 @@
+package pitta;
+
+public class perulanganDay12 {
+
+    public static void main(String[] args) {
+
+        for (int x = 1; x < 10; x++) {
+            System.out.println("Fitrah Azahrah");
+        }
+    }
+
+}
