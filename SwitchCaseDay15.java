cd3fc6c6436db9aeac92e7dc036687ad16e876c1
Fitrahazahra <116161733+Fitrahazahra@users.noreply.github.com>
2023-10-23 23:36:05 +0800

    Add files via upload

---
+++ SwitchCaseDay15.java
@@ -0,0 +1,29 @@
+package pitta;
+
+public class SwitchCaseDay15 {
+
+    public static void main(String[] args) {
+
+        int hari = 6;
+        switch (hari) {
+            case 1:
+                System.out.println("Hari ini saya makan tempe");
+                break;
+            case 2:
+                System.out.println("Hari ini saya minum susu");
+                break;
+            case 3:
+                System.out.println("Hari ini saya jalan-jalan");
+                break;
+            case 4:
+                System.out.println("Hari ini saya makan nasi goreng");
+                break;
+            case 5:
+                System.out.println("Hari ini saya minum jus");
+                break;
+            case 6:
+                System.out.println("Hari ini adalah senin");
+
+        }
+    }
+}
