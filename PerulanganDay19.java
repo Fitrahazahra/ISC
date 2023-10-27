338dd754f30d13cd171df0830a56bd6fa7f6c2ff
Fitrahazahra <116161733+Fitrahazahra@users.noreply.github.com>
2023-10-27 23:52:11 +0800

    Add files via upload

---
+++ PerulanganDay19.java
@@ -0,0 +1,13 @@
+
+package pitta;
+
+
+public class PerulanganDay19 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++){
+            System.out.println(i+"Teknik dan Informatika" );
+        }
+    }
+            
+    
+}
