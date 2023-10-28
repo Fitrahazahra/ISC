53c38360bd3d8266887c0be732cd9945ab9feb56
Fitrahazahra <116161733+Fitrahazahra@users.noreply.github.com>
2023-10-28 23:00:51 +0800

    Add files via upload

---
+++ Day20.java
@@ -0,0 +1,28 @@
+
+package pitta;
+
+public class Day20 {
+    public static void main(String[] args) {
+        boolean SIM = false;
+        boolean STNK = true;
+        
+        if(SIM == true){
+          if(STNK == true){
+        System.out.println("Anda Tidak Ditilang!");
+        
+          }
+        }else {
+            System.out.println("Anda Ditilang");
+              
+          }
+        
+        
+    }
+  
+}
+
+       
+        
+        
+            
+        
