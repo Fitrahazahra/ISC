340d90b9053f5c0d7fdff7616942a9d52cb5d9d8
Fitrahazahra <116161733+Fitrahazahra@users.noreply.github.com>
2023-10-30 02:23:36 +0800

    Add files via upload

---
+++ Day21_fittaa.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+ // Compiler version JDK 11.0.2
+
+ class Perulangan
+ {
+   public static void main(String args[]){
+     
+     
+     String[]  nama = {"Fitrah", "Lala", "Amalia"};
+     
+     for (String n : nama){
+       System.out.println(n);
+       }
+     }    
+   }
