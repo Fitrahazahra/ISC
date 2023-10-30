82659eabe9c533686c28ac8610883a0ecd06cf8e
Fitrahazahra <116161733+Fitrahazahra@users.noreply.github.com>
2023-10-30 18:31:41 +0800

    percabangan

---
+++ Day22.java
@@ -0,0 +1,23 @@
+package pitta;
+
+public class Day22 {
+
+    public static void main(String[] args) {
+        // Menentukan nama
+        String nama = "fitrah";
+        switch (nama) {
+            case "fitrah":
+                System.out.println("nama fitrah");
+                break;
+            case "akbar":
+                System.out.println("nama akbar");
+                break;
+            case "pahri":
+                System.out.println("nama pahri");
+                break;
+            default:
+                System.out.println("nama tidak diketahui");
+        }
+    }
+
+}
