4a0d8127c15051bf52600778f0300f0da49753e8
Fitrahazahra <116161733+Fitrahazahra@users.noreply.github.com>
2023-10-31 20:01:57 +0800

    percabangan

---
+++ Day23.java
@@ -0,0 +1,15 @@
+package pitta;
+
+public class Day23 {
+
+    public static void main(String[] args) {
+
+        // Mengecek apakah bilangan lebih besar dari 20
+        int bilangan = 30;
+        if (bilangan > 20) {
+            System.out.println("Bilangan lebih besar dari 20");
+        } else {
+            System.out.println("Bilangan kurang dari atau sama dengan 20");
+        }
+    }
+}
