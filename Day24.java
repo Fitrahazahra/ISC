7cb431d0773391b52ab6d6a966a34c90159dba83
Fitrahazahra <116161733+Fitrahazahra@users.noreply.github.com>
2023-11-01 20:43:33 +0800

    perulangan

---
+++ Day24.java
@@ -0,0 +1,13 @@
+package pitta;
+
+public class Day24 {
+
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println("Iterasi ke-" + i);
+            i++;
+        } while (i <= 50);
+
+    }
+}
