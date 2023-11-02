4d08a9f14cdd5af5f9e805d18bdffe2f6b0668fe
Fitrahazahra <116161733+Fitrahazahra@users.noreply.github.com>
2023-11-02 23:18:20 +0800

    percabangan

---
+++ Day25.java
@@ -0,0 +1,16 @@
+package pitta;
+
+public class Day25 {
+
+    public static void main(String[] args) {
+        int nilai1 = 25;
+        if (nilai1 > 30) {
+            System.out.println("Nilai lebih dari 30");
+        } else if (nilai1 > 10) {
+            System.out.println("Nilai lebih dari 10");
+        } else {
+            System.out.println("Nilai kurang dari atau sama dengan 10");
+        }
+
+    }
+}
